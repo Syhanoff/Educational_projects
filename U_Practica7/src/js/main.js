59799811fc8942f8d4359ddb7d6d7a49cbdbbe8d
Stanislav <s.syhanof@gmail.com>
2023-09-04 10:00:18 +0300

    Добавлена файл README и реализована работа табов

--- U_Practica7/src/js/main.js
+++ U_Practica7/src/js/main.js
@@ -1,1 +1,35 @@
 'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  //tabs
+  const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsWrapper = document.querySelector('.tabheader__items'),
+        tabContent = document.querySelectorAll('.tabcontent');
+
+  function hideTabContent() {
+    tabContent.forEach(item => item.style.display = 'none');
+    tabs.forEach(tab => tab.classList.remove('tabheader__item_active'));
+  }
+  hideTabContent();
+  
+  function activeTabAndShowContent(i = 0) {
+    tabContent[i].style.display = 'block';
+    tabs[i].classList.add('tabheader__item_active');
+  }
+  activeTabAndShowContent();
+
+  tabsWrapper.addEventListener('click', (event) => {
+    if(event.target.classList.contains('tabheader__item')) {
+      tabs.forEach((tab, i) => {
+        if(event.target == tab) {          
+          hideTabContent();
+          activeTabAndShowContent(i);
+        }
+      })
+    }
+  })
+
+
+
+})
