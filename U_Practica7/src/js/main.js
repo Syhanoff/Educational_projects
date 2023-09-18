70f28e62254cf0a25e8e3e6ced36df3c0a9ff7b2
Stanislav <s.syhanof@gmail.com>
2023-09-18 12:22:01 +0300

    Добавил скрипт для слайдера

--- U_Practica7/src/js/main.js
+++ U_Practica7/src/js/main.js
@@ -53,6 +53,16 @@ window.addEventListener('DOMContentLoaded', () => {
     };
   }
 
+  function setZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else if (num < 0) {
+      return num = 0;
+    } else {
+      return num;
+    };
+  };
+
   function setTime(clockBox, end) {
     const timer = document.querySelector(clockBox),
           days = timer.querySelector('#days'),
@@ -63,15 +73,6 @@ window.addEventListener('DOMContentLoaded', () => {
     
     updateTime();
 
-    function setZero(num) {
-      if (num >= 0 && num < 10) {
-        return `0${num}`;
-      } else if (num < 0) {
-        return num = 0;
-      } else {
-        return num;
-      };
-    };
 
     function updateTime() {
       const returnTime = getRestTime(end);
@@ -159,13 +160,15 @@ window.addEventListener('DOMContentLoaded', () => {
   buttonsOpenModal.forEach(el => el.addEventListener('click', openModal));
   const modalInterval = setTimeout(openModal, 15000);
   window.addEventListener('scroll', showModalByScroll);
-
+  
   function showModalByScroll() {
-    if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+    if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
       openModal();
       window.removeEventListener('scroll', showModalByScroll);
     }
   };
+  
+  // document.documentElement.scrollTop;
 
   function openModal() {
     modal.classList.add('active');
@@ -272,4 +275,94 @@ window.addEventListener('DOMContentLoaded', () => {
       document.body.style.overflow = '';
     }, 4000);
   }
+
+  //Slider
+  const btnPrev = document.querySelector('.offer__slider-prev'),
+        btnNext = document.querySelector('.offer__slider-next'),
+        currentNumberSlid = document.querySelector('#current'),
+        totalNumberSlid = document.querySelector('#total'),
+        slideWrapper = document.querySelector('.offer__slider-wrapper'),
+        sliderInnerWrapper = document.querySelector('.offer__slider-inner'),
+        width = window.getComputedStyle(slideWrapper).width,
+        slider = document.querySelector('.offer__slider'),
+        sliders = document.querySelectorAll('.offer__slide');
+        
+  sliderInnerWrapper.style.width = 100 * sliders.length + '%';
+  sliders.forEach(slide => {
+    slide.style.width = width;
+  });
+
+  let currentIndex = 1;
+  let offset = 0;
+  currentNumberSlid.innerText = setZero(currentIndex);
+  totalNumberSlid.innerText = setZero(sliders.length);
+
+  btnNext.addEventListener('click', () => {
+    if (offset == parseInt(width) * (sliders.length - 1)) {
+      offset = 0;
+    } else {
+      offset += parseInt(width);
+    };
+    sliderInnerWrapper.style.transform = `translateX(-${offset}px)`;
+
+    if (currentIndex == sliders.length) {
+      currentIndex = 1;
+    } else {
+      currentIndex++;
+    };
+    currentNumberSlid.innerText = setZero(currentIndex);
+
+    activeDot();
+  })
+
+  btnPrev.addEventListener('click', () => {
+    if (offset == 0) {
+      offset = parseInt(width) * (sliders.length - 1);
+    } else {
+      offset -= parseInt(width);
+    };
+    sliderInnerWrapper.style.transform = `translateX(-${offset}px)`;
+
+    if (currentIndex == 1) {
+      currentIndex = sliders.length;
+    } else {
+      currentIndex--;
+    };
+    currentNumberSlid.innerText = setZero(currentIndex);
+
+    activeDot();
+  });
+
+  const indicators = document.createElement('ol');
+  indicators.className = 'carousel-indicators';
+  slider.append(indicators);
+  
+  const dots = [];
+  
+  for (let i = 0; i < sliders.length; i++) {
+    const dot = document.createElement('li');
+    dot.setAttribute('data-slide-to', i + 1)
+    dot.className = 'dot';
+    if (i == 0) {
+      dot.style.opacity = 1;
+    };
+    indicators.append(dot);
+    dots.push(dot);
+  };
+
+  function activeDot() {
+    dots.forEach(dot => dot.style.opacity = '.5');
+    dots[currentIndex - 1].style.opacity = 1;
+  };
+
+  dots.forEach(dot => {
+    dot.addEventListener('click', (e) => {
+      const slideTo = e.target.getAttribute('data-slide-to');
+      currentIndex = slideTo;
+      offset = parseInt(width) * (slideTo - 1);
+      sliderInnerWrapper.style.transform = `translateX(-${offset}px)`;
+      currentNumberSlid.innerText = setZero(currentIndex);
+      activeDot();
+    });
+  });
 })
